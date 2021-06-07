eec909831625eba67a1933400bb7e0dd11bd2d6c
waelsaidi <51228799+waelsaidi@users.noreply.github.com>
2021-06-07 10:30:59 +0100

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -7,3 +7,18 @@ CREATE TABLE Employees
       PRIMARY KEY CLUSTERED (EmployeeID)
     )
 GO
+
+
+CREATE TABLE EmployeesAudit
+    (
+      AuditID INTEGER NOT NULL IDENTITY(1, 1) ,
+      EmployeeID INTEGER ,
+      EmployeeName VARCHAR(50) ,
+      EmployeeAddress VARCHAR(50) ,
+      MonthSalary NUMERIC(10, 2) ,
+      ModifiedBy VARCHAR(128) ,
+      ModifiedDate DATETIME ,
+      Operation CHAR(1) 
+      PRIMARY KEY CLUSTERED ( AuditID )
+    )
+GO
