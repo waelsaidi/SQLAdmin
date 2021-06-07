5e59db992cb1291eafebe97c4c4164c774285b0a
waelsaidi <51228799+waelsaidi@users.noreply.github.com>
2021-06-07 13:37:29 +0100

    Create HandleError.sql

---
+++ HandleError.sql
@@ -0,0 +1,24 @@
+ 
+CREATE TABLE DB_Errors
+         (ErrorID        INT IDENTITY(1, 1),
+          UserName       VARCHAR(100),
+          ErrorNumber    INT,
+          ErrorState     INT,
+          ErrorSeverity  INT,
+          ErrorLine      INT,
+          ErrorProcedure VARCHAR(MAX),
+          ErrorMessage   VARCHAR(MAX),
+          ErrorDateTime  DATETIME)
+GO
+
+
+NSERT INTO dbo.DB_Errors
+    VALUES
+  (SUSER_SNAME(),
+   ERROR_NUMBER(),
+   ERROR_STATE(),
+   ERROR_SEVERITY(),
+   ERROR_LINE(),
+   ERROR_PROCEDURE(),
+   ERROR_MESSAGE(),
+   GETDATE());
